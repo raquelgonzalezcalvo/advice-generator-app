5219f44470b113f37d669f9a06015f561a052869
raquelgonzalezcalvo <ragoncal13@gmail.com>
2023-06-08 14:20:51 +0200

    finish

--- docs/assets/js/main.js
+++
@@ -1,1 +0,0 @@
-"use strict";const title=document.querySelector(".js-title"),text=document.querySelector(".js-paragraph"),url="https://api.adviceslip.com/advice",button=document.querySelector(".js-button"),getAdvice=()=>{fetch(url).then(t=>t.json()).then(t=>{title.innerHTML="ADVICE #"+t.slip.id,text.innerHTML=`"${t.slip.advice}"`})};button.addEventListener("click",getAdvice);
