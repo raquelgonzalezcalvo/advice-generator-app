46e5f86b6710801a832ba68b91aa9dbd426d8f93
raquelgonzalezcalvo <ragoncal13@gmail.com>
2023-06-08 14:52:17 +0200

    Update docs

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,16 +1,1 @@
-"use strict";
-const title = document.querySelector(".js-title");
-const text = document.querySelector(".js-paragraph");
-const url = "https://api.adviceslip.com/advice";
-const button = document.querySelector(".js-button");
-
-const getAdvice = () => {
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      title.innerHTML = `ADVICE #${data.slip.id}`;
-      text.innerHTML = `"${data.slip.advice}"`;
-    });
-};
-
-button.addEventListener("click", getAdvice);
+"use strict";const title=document.querySelector(".js-title"),text=document.querySelector(".js-paragraph"),url="https://api.adviceslip.com/advice",button=document.querySelector(".js-button"),getAdvice=()=>{fetch(url).then(t=>t.json()).then(t=>{title.innerHTML="ADVICE #"+t.slip.id,text.innerHTML=`"${t.slip.advice}"`})};button.addEventListener("click",getAdvice);
